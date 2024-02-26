aac2faa45b7c7ff950679f16410befa96f9dd0e3
lbackman <lucasbackman@gmail.com>
2024-02-26 15:57:58 +0200

    Add default books with javascript instead

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const myLibrary = [];
 
 function Book(book, author, pages, read) {
-  // book constructor
+  this.book = book
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+const silmarillion = new Book('The Silmarillion', 'J.R.R Tolkien', 365, true)
+const lostTime = new Book('In Search of Lost Time', 'Marcel Proust', 4215, false)
+
+myLibrary.push(silmarillion)
+myLibrary.push(lostTime)
+
+const template = function(idx, book, author, pages, read) {
+  return `<tr data-attribute="${idx}">
+    <td>${book}</td>
+    <td>${author}</td>
+    <td>${pages}</td>
+    <td>${read === true ? "Yes" : "No"}</td>
+    <td><button class="toggle-read">Mark read</button></td>
+    <td><button class="delete">Delete</button></td>
+    </tr>`
+}
+
+const library = document.querySelector('.library tbody')
+myLibrary.forEach((title, idx) => {
+  library.insertAdjacentHTML('beforeend', template(idx, title.book, title.author, title.pages, title.read))
+})
+
+Book.prototype.toggleStatus = function() {
+  console.log(this)
+  this.read = !this.read
 }
 
 function addBookToLibrary() {
